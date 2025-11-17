f8454a51f58f5ce5007774df66a275bb117286cc
LuongQuocDong <luonggrim@gmail.com>
2025-11-17 16:05:19 +0700

    chore: rename and setup NaLumosShop-admin

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/veggie-shop-admin'),
+      dir: require('path').join(__dirname, './coverage/nalumosshop-admin'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'veggie-shop-admin'`, () => {
+  it(`should have as title 'NaLumosShop-admin'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('veggie-shop-admin');
+    expect(app.title).toEqual('NaLumosShop-admin');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('veggie-shop-admin app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('NaLumosShop-admin app is running!');
   });
 });
